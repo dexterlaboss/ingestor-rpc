ae5a7cccc862368177d958600d6194e1ad8cf9c9
aurimas <aurimas.n@dexterlab.com>
2024-10-04 00:15:04 +0300

    Fix latest stored slot retrieval

--- src/hbase.rs
+++ src/hbase.rs
@@ -178,6 +178,7 @@ impl HBase {
         start_at: Option<RowKey>,
         end_at: Option<RowKey>,
         rows_limit: i64,
+        reversed: bool,
     ) -> Result<Vec<RowKey>> {
         if rows_limit == 0 {
             return Ok(vec![]);
@@ -196,6 +197,8 @@ impl HBase {
         scan.batch_size = Some(rows_limit as i32);
         scan.timestamp = None;
         scan.caching = rows_limit.try_into().ok();
+        scan.reversed = Some(reversed);
+        scan.filter_string = Some(b"KeyOnlyFilter()".to_vec());
 
         let scan_id = self.client.scanner_open_with_scan(
             table_name.as_bytes().to_vec(),
@@ -450,8 +453,7 @@ impl HBase {
     }
 
     pub async fn get_last_row_key(&mut self, table_name: &str) -> Result<String> {
-        let row_keys = self.get_row_keys(table_name, None, None, 1).await?;
-
+        let row_keys = self.get_row_keys(table_name, None, None, 1, true).await?;
         if let Some(last_row_key) = row_keys.first() {
             Ok(last_row_key.clone())
         } else {
